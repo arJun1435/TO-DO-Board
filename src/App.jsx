2f77d8362f6183f93e10e7f9061dcb6fbb783552
arJun1435 <malliymr1435@gmail.com>
2025-02-07 13:36:09 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <button className='btn btn-primary m-5'>Submit</button>
+    </div>
+  )
+}
+
+export default App
